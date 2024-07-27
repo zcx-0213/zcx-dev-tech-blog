ec7ccf2c09af9209636dec40485f490280952ae0
zcx-0213 <zhangchuxin0213@163.com>
2024-07-27 20:11:30 +0800

    Update sidebar.ts
    啊啊啊啊

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -4,7 +4,7 @@ export default sidebar({
   "/": [
     "",
     {
-      text: "Lottery 项目开发日志",
+      text: "开发笔记",
       icon: "laptop-code",
       prefix: "md/dev-log/",
       children: ["day01.md","day02.md","day03.md","day04.md","day05.md","day06.md","day07~day08.md","day09~day10.md","day11.md","day12.md"],
